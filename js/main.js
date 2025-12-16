7eb4d79cd4328d24543f33e255f077a7a4e908bc
Manuel Gozzi <manuel2006gozzi@gmail.com>
2025-12-16 15:54:11 +0100

    Prompt

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+const myKm = parseInt(prompt("inserisci qui i chilometri"));
+const myAge = parseInt(prompt("inserisci qui l'età"));
+if (isNaN(myKm) || isNaN(myAge)) {
+  alert("Required Real NUMBER");
+}
+console.log("myKm", myKm);
+console.log("myAge", myAge);
