8e6414d139662df6ca6cdaa579706b0659ca69f5
Manuel Gozzi <manuel2006gozzi@gmail.com>
2025-12-16 16:28:43 +0100

    underage sales

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 // Chiedo all'utente l'età
 // const myAge = parseInt(prompt("inserisci qui l'età"));
 const myKm = parseInt(30);
-const myAge = parseInt(30);
+const myAge = parseInt(19);
 //     VALIDAZIONE IN NUMERI
 if (isNaN(myKm) || isNaN(myAge)) {
   alert("Required Real NUMBER");
@@ -13,8 +13,14 @@ console.log("myKm", myKm);
 console.log("myAge", myAge);
 
 // Calcolo il prezzo del tragitto in base ai km
-console.log("costo al km", myKm * 0.21);
+let myPrice = myKm * 0.21;
+console.log("costo al km", myPrice);
 //     SE sotto i 18 sconto 20%
+if (myAge < 18) {
+  alert("il prezzo è questo" + " = " + (myPrice * 20) / 100);
+} else {
+  alert("il prezzo è questo" + " = " + myPrice);
+}
 //     ALTRIMENTI SE sopra i 65 sconto 40%
 //     ALTRIMENTI prezzo completo
 
